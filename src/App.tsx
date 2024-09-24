98529cfef2eba41b28c2153ca8f29d556cc736e6
草をこして森 <kyou0317kyou0317@gmail.com>
2024-09-25 00:06:33 +0900

    1か月タイムカプセル

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+//cssの競合が起きたため終了。Reactの基本的な考え方(親コンポーネントに要素を入れる。cssの書き方などのことを調べて他の物を制作してみる)
+//未来のオレ頑張ってくれ。私は疲れた。自分ならできる。ワイには応援することしかできないけど... こんなの書いてる暇あったら勉強しよ
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Top from "./views/Top"; // 新しく作成したHomeコンポーネントをインポート
